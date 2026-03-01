3c9a592ba5119012c19c53edc973b945511e1048
JacobHelwig <jacob.a.helwig@gmail.com>
2026-02-28 19:13:39 -0600

    Auto await

--- verl/experimental/agent_loop/agent_loop.py
+++ verl/experimental/agent_loop/agent_loop.py
@@ -1048,7 +1048,7 @@ class AgentLoopManager:
             DataProto: Output batch.
         """
         if self.distillation_enabled:  # TODO: need same for reward loop workers
-            self.teacher_loop_manager.wake_up()
+            await self.teacher_loop_manager.wake_up()
         chunkes = prompts.chunk(len(self.agent_loop_workers))
         outputs = await asyncio.gather(
             *[
@@ -1057,7 +1057,7 @@ class AgentLoopManager:
             ]
         )
         if self.distillation_enabled:  # TODO: need same for reward loop workers
-            self.teacher_loop_manager.sleep()
+            await self.teacher_loop_manager.sleep()
         output = DataProto.concat(outputs)
 
         # calculate performance metrics
